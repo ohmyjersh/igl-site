3b42f8650cd1d5ad18ac6117597de02427d8de44
Josh Jarmain <jjarmain@gmail.com>
2017-11-16 20:15:45 -0800

    init

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        Influential Grooming Lounge site goes here.
+      </div>
+    );
+  }
+}
+
+export default App;
