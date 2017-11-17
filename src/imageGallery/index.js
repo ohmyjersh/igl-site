d02b6fc6a5e7b1955692614e180b2eee06b9c82b
JJ <jjarmain@gmail.com>
2017-11-17 12:16:43 -0800

    add gallery and new router

--- src/imageGallery/index.js
+++ src/imageGallery/index.js
@@ -1,31 +1,65 @@
 import React from 'react';
-import ImageGallery from 'react-image-gallery';
-import "../../node_modules/react-image-gallery/styles/css/image-gallery-no-icon.css";
+import Gallery from 'react-photo-gallery';
+import Lightbox from 'react-images';
 
-class Gallery extends React.Component {
+const photos = [
+  {src: 'https://source.unsplash.com/2ShvY8Lf6l0/800x599', width:4, height:3},
+  {src: 'https://source.unsplash.com/Dm-qxdynoEc/800x799', width:1, height:1},
+  {src: 'https://source.unsplash.com/qDkso9nvCg0/600x799', width:3, height:4},
+  {src: 'https://source.unsplash.com/iecJiKe_RNg/600x799', width:3, height:4},
+  {src: 'https://source.unsplash.com/epcsn8Ed8kY/600x799', width:3, height:4},
+  {src: 'https://source.unsplash.com/NQSWvyVRIJk/800x599', width:4, height:3},
+  {src: 'https://source.unsplash.com/zh7GEuORbUw/600x799', width:3, height:4},
+  {src: 'https://source.unsplash.com/PpOHJezOalU/800x599', width:4, height:3},
+  {src: 'https://source.unsplash.com/I1ASdgphUH4/800x599', width:4, height:3}
+];
 
-  render() {
-
-    const images = [
-      {
-        original: 'http://lorempixel.com/1000/600/nature/1/',
-        thumbnail: 'http://lorempixel.com/250/150/nature/1/',
-      },
-      {
-        original: 'http://lorempixel.com/1000/600/nature/2/',
-        thumbnail: 'http://lorempixel.com/250/150/nature/2/'
-      },
-      {
-        original: 'http://lorempixel.com/1000/600/nature/3/',
-        thumbnail: 'http://lorempixel.com/250/150/nature/3/'
-      }
-    ]
-
-    return (
-      <ImageGallery items={images} />
-    );
+class Galley extends React.Component{
+  constructor(){             
+    super();                 
+    this.state = { currentImage: 0 }; 
+    this.closeLightbox = this.closeLightbox.bind(this); 
+    this.openLightbox = this.openLightbox.bind(this);
+    this.gotoNext = this.gotoNext.bind(this);
+    this.gotoPrevious = this.gotoPrevious.bind(this);
   }
-
+  openLightbox(event, obj) {
+    console.log('open')
+    this.setState({
+      currentImage: obj.index,
+      lightboxIsOpen: true,
+    });  
+  }
+  closeLightbox() {
+    this.setState({
+      currentImage: 0,
+      lightboxIsOpen: false,
+    }); 
+  }
+  gotoPrevious() {
+    this.setState({
+      currentImage: this.state.currentImage - 1,                                                           
+    });  
+  }
+  gotoNext() {
+    this.setState({
+      currentImage: this.state.currentImage + 1,                                            
+    }); 
+  }
+  render(){
+    return(
+      <div>
+        <Gallery photos={photos} onClick={this.openLightbox} />
+        <Lightbox images={photos}
+          onClose={this.closeLightbox}
+          onClickPrev={this.gotoPrevious}
+          onClickNext={this.gotoNext}
+          currentImage={this.state.currentImage}
+          isOpen={this.state.lightboxIsOpen}
+        />
+      </div>
+    )
+  }
 }
 
-export default Gallery;
+export default Galley;
