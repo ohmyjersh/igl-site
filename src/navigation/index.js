107f6a6c16dafde65c536bcaae73cfe836d7510d
Josh Jarmain <jjarmain@gmail.com>
2017-11-18 10:54:08 -0800

    responsive stuff for phones

--- src/navigation/index.js
+++ src/navigation/index.js
@@ -3,15 +3,15 @@ import {Link} from 'react-router-dom';
 import './nav.css';
 
 export default () => (
-    <nav className="flexContainer blueBackground">
-    <ul className="nav flexContainer flexItem flexStart">
-        <li><Link to="/">Influential Grooming Lounge</Link></li>    
-    </ul>
-    <ul className="nav flexContainer flexEnd">   
-        <li><Link to="/about">About</Link></li>
-        <li><Link to="/book">Book</Link></li>
-        <li><Link to="/gallery">Gallery</Link></li>
-        <li><Link to="/contact">Contact</Link></li>
-    </ul>
-</nav>
+    <nav className="flexContainer">
+        <ul className="nav flexContainer flexItem flexStart">
+            <li><Link to="/">Influential Grooming Lounge</Link></li>    
+        </ul>
+        <ul className="nav flexContainer flexEnd">   
+            <li><Link to="/about">About</Link></li>
+            <li><Link to="/book">Book</Link></li>
+            <li><Link to="/gallery">Gallery</Link></li>
+            <li><Link to="/contact">Contact</Link></li>
+        </ul>
+    </nav>
 )
